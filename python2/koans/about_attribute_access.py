d254ab7784a6579f5633ffdb9a9d8d3fcfb45b5a
shivakrishnab <shiva@agiliq.com>
2013-06-26 17:42:54 +0530

    Altered travis.yml and Fixed about_attribute_access.AboutAttributeAccess.test_setattr_intercepts_attribute_assignments

--- python2/koans/about_attribute_access.py
+++ python2/koans/about_attribute_access.py
@@ -196,13 +196,14 @@ class AboutAttributeAccess(Koan):
         fanboy.comic = 'The Laminator, issue #1'
         fanboy.pie = 'blueberry'
 
-        self.assertEqual(__, fanboy.a_pie)
+        # self.assertEqual(__, fanboy.a_pie)
+        self.assertEqual('blueberry', fanboy.a_pie)
 
         #
         # NOTE: Change the prefix to make this next assert pass
         #
 
-        prefix = '__'
+        prefix = 'my'
         self.assertEqual(
             "The Laminator, issue #1",
             getattr(fanboy, prefix + '_comic'))
