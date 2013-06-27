114b82bbfa888ed876aa7748f1320932250f5d9f
shivakrishnab <shiva@agiliq.com>
2013-06-27 18:52:57 +0530

    Fixed about_generators.AboutGenerators.test_generators_can_see_if_they_have_been_called_with_a_value

--- python2/koans/about_generators.py
+++ python2/koans/about_generators.py
@@ -137,7 +137,8 @@ class AboutGenerators(Koan):
 
         generator2 = self.yield_tester()
         next(generator2)
-        self.assertEqual(__, next(generator2))
+        # self.assertEqual(__, next(generator2))
+        self.assertEqual('no value', next(generator2))
 
     def test_send_none_is_equivalent_to_next(self):
         generator = self.yield_tester()
