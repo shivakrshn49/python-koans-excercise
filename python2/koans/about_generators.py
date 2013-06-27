784aee8304612ee148403eb4790251ba4f399456
shivakrishnab <shiva@agiliq.com>
2013-06-27 18:25:35 +0530

    Fixed about_generators.AboutGenerators.test_coroutines_can_take_arguments

--- python2/koans/about_generators.py
+++ python2/koans/about_generators.py
@@ -63,8 +63,10 @@ class AboutGenerators(Koan):
 
     def test_coroutines_can_take_arguments(self):
         result = self.simple_generator_method()
-        self.assertEqual(__, next(result))
-        self.assertEqual(__, next(result))
+        # self.assertEqual(__, next(result))
+        # self.assertEqual(__, next(result))
+        self.assertEqual('peanut', next(result))
+        self.assertEqual('butter', next(result))
         result.close()
 
     # ------------------------------------------------------------------
