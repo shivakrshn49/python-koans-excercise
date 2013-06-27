1459a0efc19451ad2b00edf3019a95dceaf64d61
shivakrishnab <shiva@agiliq.com>
2013-06-27 17:40:48 +0530

    Fixed about_dictionaries.AboutDictionaries.test_changing_dictionaries

--- python2/koans/about_dictionaries.py
+++ python2/koans/about_dictionaries.py
@@ -33,7 +33,7 @@ class AboutDictionaries(Koan):
         babel_fish = {'one': 'uno', 'two': 'dos'}
         babel_fish['one'] = 'eins'
 
-        expected = {'two': 'dos', 'one': __}
+        expected = {'two': 'dos', 'one': 'eins'}
         self.assertEqual(expected, babel_fish)
 
     def test_dictionary_is_unordered(self):
