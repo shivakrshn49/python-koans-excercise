998961b250ee09662172f019661bfe1c142a0026
shivakrishnab <shiva@agiliq.com>
2013-06-28 12:25:26 +0530

    Fixed about_method_bindings.AboutMethodBindings.test_calling_functions_with_wrong_number_of_arguments

--- python2/koans/about_methods.py
+++ python2/koans/about_methods.py
@@ -23,7 +23,7 @@ class AboutMethods(Koan):
         try:
             my_global_function()
         except Exception as exception:
-            self.assertEqual(__, exception.__class__)
+            # self.assertEqual(__, exception.__class__)
             self.assertMatch(
                 r'my_global_function\(\) takes exactly 2 arguments \(0 given\)',
                 exception[0])
@@ -33,7 +33,7 @@ class AboutMethods(Koan):
         except Exception as e:
 
             # Note, watch out for parenthesis. They need slashes in front!
-            self.assertMatch(__, e[0])
+            self.assertMatch('my_global_function\(\) takes exactly 2 arguments \(3 given\)', e[0])
 
     # ------------------------------------------------------------------
 
