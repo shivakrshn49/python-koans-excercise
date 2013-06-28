dda2777e66b0da5ff341e573ddd4deb75780d167
shivakrishnab <shiva@agiliq.com>
2013-06-28 13:15:57 +0530

    Fixed about_multiple_inheritance.AboutMultipleInheritance.test_base_class_methods_are_also_available_in_the_object

--- python2/koans/about_multiple_inheritance.py
+++ python2/koans/about_multiple_inheritance.py
@@ -96,7 +96,7 @@ class AboutMultipleInheritance(Koan):
             jeff.set_name("Rover")
         except:
             self.fail("This should not happen")
-        self.assertEqual(____, jeff.can_climb_walls())
+        self.assertEqual(True, jeff.can_climb_walls())
 
     def test_base_class_methods_can_affect_instance_variables_in_the_object(self):
         jeff = self.Spiderpig()
