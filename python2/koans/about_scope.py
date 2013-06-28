b410dc1d00f0056f3bacbebf9071409c15540bfb
shivakrishnab <shiva@agiliq.com>
2013-06-28 16:37:28 +0530

    Fixed about_scope.AboutScope.test_constants_are_defined_with_an_initial_uppercase_letter

--- python2/koans/about_scope.py
+++ python2/koans/about_scope.py
@@ -52,7 +52,7 @@ class AboutScope(Koan):
     PI = 3.1416
 
     def test_constants_are_defined_with_an_initial_uppercase_letter(self):
-        self.assertAlmostEqual(_____, self.PI)
+        self.assertAlmostEqual(3.1416, self.PI)
         # Note, floating point numbers in python are not precise.
         # assertAlmostEqual will check that it is 'close enough'
 
