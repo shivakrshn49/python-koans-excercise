01e3f907b822ecb6816098c9f68f3b1e0bd6fc2e
shivakrishnab <shiva@agiliq.com>
2013-06-28 16:44:23 +0530

    Fixed about_sets.AboutSets.test_convert_the_set_into_a_list_to_sort_it

--- python2/koans/about_sets.py
+++ python2/koans/about_sets.py
@@ -17,7 +17,7 @@ class AboutSets(Koan):
         self.assertEqual(set(['1', '3', '2', '5', '4']), set('12345'))
 
     def test_convert_the_set_into_a_list_to_sort_it(self):
-        self.assertEqual(__, sorted(set('13245')))
+        self.assertEqual(['1', '2', '3', '4', '5'], sorted(set('13245')))
 
     # ------------------------------------------------------------------
 
